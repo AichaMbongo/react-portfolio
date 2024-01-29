7e154eb154745baf9bd20f17f091229064c1fb9b
Aicha Mbongo <zindamoyen2@gmail.com>
2024-01-29 21:26:35 +0300

    chore: Adjust ImageCard component code with optional link
    This commit updates the ImageCard component to accept an optional link prop, providing flexibility for including or excluding external links in the rendered component. This change enhances the reusability of the ImageCard component and allows better customization based on specific use cases.

--- my-portfolio/src/components/ImageCard.js
+++ my-portfolio/src/components/ImageCard.js
@@ -1,15 +1,44 @@
 // ImageCard.js
-import React from 'react';
+// Import React
+import React, { useState } from 'react';
 
+// Define the component
 const ImageCard = ({ imageSrc, title, description, link }) => {
+  // State to manage visibility
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  // Function to toggle visibility
+  const toggleText = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className="col-md-3 mb-4">
       <div className="card p-1 bg-dark text-white shadow-lg">
         <img src={imageSrc} className="card-img-top" alt="..." />
         <div className="card-body">
           <p className="card-text">
-            {description} <br />
-            {link && <a href={link} target="_blank">Click to View Project</a>}
+            {description}
+            {isExpanded && (
+              <>
+                <br />
+                <a onClick={toggleText} className="text-primary">
+                  Read Less
+                </a>
+              </>
+            )}
+            {!isExpanded && (
+              <>
+                <br />
+                {/* <a onClick={toggleText} className="text-primary">
+                  Read More
+                </a> */}
+              </>
+            )}
+            <br />
+            <a href={link} target="_blank">
+              View project
+            </a>
           </p>
         </div>
       </div>
