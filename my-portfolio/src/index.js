2d4b1f5e410e4cfe8e87ff9867526cfbde687a6d
Aicha Mbongo <zindamoyen2@gmail.com>
2024-01-29 15:31:22 +0300

    feat: Create reusable Carousel component
    This commit introduces a new React component named 'Carousel'.
    The component is designed to be a reusable carousel/slider that can
    display images with optional captions.

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar';
+import Carousel from './components/Carousel';
 import Section from './components/section';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Carousel />
+
+     
+
      
     </div>
   );
